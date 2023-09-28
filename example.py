e3e7b1f76c06cdb022fa4f8d13caa4403388f211
LouisSimon <45872807+FastAndFourier@users.noreply.github.com>
2023-09-28 09:50:35 +0200

    adding few tests

--- example.py
+++ example.py
@@ -6,3 +6,12 @@ def subtract(a, b):
 
 def multiply(a, b):
     return a * b
+
+def test_add(a, b):
+    assert add(2,3) == 5
+
+def test_subtract(a, b):
+    assert substract(2,3) == -1
+    
+def test_multiply(a, b):
+    assert multiply(2, -1) == 2
